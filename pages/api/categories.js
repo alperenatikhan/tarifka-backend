b194398790e29750340b0aa7a3aa3269ccec6b65
alp <alperenmyung@gmail.com>
2022-08-10 17:56:32 +0200

    cors fixed in categories

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,8 +1,25 @@
 import axios from 'axios'
 
+import Cors from 'cors'
 
 
-export default async function handler(req, res) {
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+
+function runMiddleware(req,res,fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req,res) {
+
+  await runMiddleware(req, res, cors)
 
 let categories = await axios.get('http://www.themealdb.com/api/json/v1/1/list.php?c=list').then(item=> item.data).then(item=> item.meals)
 
